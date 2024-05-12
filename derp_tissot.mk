9e214373e8f0fd49940cf15c80a5b058c3c20f6d
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-05-12 12:49:24 +0600

    Update derp_tissot.mk

--- derp_tissot.mk
+++ derp_tissot.mk
@@ -1,22 +1,35 @@
 #
-# Copyright (C) 2017-2021 The LineageOS Project
+# Copyright (C) 2023 The Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
+DEVICE_PATH := device/xiaomi/tissot
+
+# Configure core_64_bit.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Configure full_base_telephony.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common derp stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+# Inherit common Derpfest configurations
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+# Inherit device configurations
+$(call inherit-product, $(DEVICE_PATH)/device.mk)#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from tissot device
 AB_OTA_UPDATER := true
 TARGET_IS_LEGACY := true
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
-# Device identifier. This must come after all inclusions
+# Derpfest
+DERP_BUILDTYPE := Unofficial
+
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
 PRODUCT_NAME := derp_tissot
 BOARD_VENDOR := Xiaomi
@@ -25,14 +38,14 @@ PRODUCT_MODEL := Mi A1
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
 
-# LineageOS Stuffs
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_QUICK_TAP := true
+#GApps
+TARGET_USES_PICO_GAPPS := true
+
+# GMS
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
