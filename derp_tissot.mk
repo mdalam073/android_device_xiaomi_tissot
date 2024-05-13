9b962b5945b7825f0917010626fba626975ab3d1
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-05-13 15:45:41 +0600

    Update derp_tissot.mk

--- derp_tissot.mk
+++ derp_tissot.mk
@@ -1,35 +1,30 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2017 The LineageOS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-
-DEVICE_PATH := device/xiaomi/tissot
-
-# Configure core_64_bit.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-# Configure full_base_telephony.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit common Derpfest configurations
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
-
-# Inherit device configurations
-$(call inherit-product, $(DEVICE_PATH)/device.mk)#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from tissot device
-AB_OTA_UPDATER := true
-TARGET_IS_LEGACY := true
+# Inherit from mido device
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
-# Derpfest
-DERP_BUILDTYPE := Unofficial
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
-## Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
 PRODUCT_NAME := derp_tissot
 BOARD_VENDOR := Xiaomi
@@ -37,6 +32,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
+DERP_BUILDTYPE := Official
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
@@ -44,8 +42,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
 
-#GApps
-TARGET_USES_PICO_GAPPS := true
-
-# GMS
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+# PixelExperience Stuffs
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
