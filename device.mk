3a5d22c672f90d160953b83dee2153bbb88cafed
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-05-31 05:10:16 +0600

    Update device.mk

--- device.mk
+++ device.mk
@@ -11,11 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-superior
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
