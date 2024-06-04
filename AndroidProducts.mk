c45ec323db7b3300ec131bf10473360f0b2d7ea1
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-06-04 16:27:46 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/superior_tissot.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_tissot-ap1a-eng \
+    superior_tissot-user \
     superior_tissot-ap1a-userdebug \
-    superior_tissot-ap1a-user
+    superior_tissot-eng
