5d6bf638cb55840dcc19a4795ccee496509f7f1b
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-06-12 20:37:50 +0600

    Update voltage_tissot.mk

--- voltage_tissot.mk
+++ voltage_tissot.mk
@@ -10,6 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common VoltageOS stuff.
 $(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1920
 
 # Inherit from tissot device
 AB_OTA_UPDATER := true
