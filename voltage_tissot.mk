508150c2d3f77a057e9c887e253beb22cc81e726
mdalam073 <md.alam073@gmail.com>
2024-11-11 10:38:09 +0600

    bringups for voltage

--- voltage_tissot.mk
+++ voltage_tissot.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2021 The LineageOS Project
+# Copyright (C) 2024 The VoltageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Inherit from tissot device
 AB_OTA_UPDATER := true
@@ -18,7 +18,7 @@ $(call inherit-product, device/xiaomi/tissot/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := voltage_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
@@ -33,6 +33,17 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
 
-# LineageOS Stuffs
-TARGET_GAPPS_ARCH := arm64
+#VoltageOS Stuff
+VOLTAGE_BUILD_TYPE := UNOFFICIAL
+TARGET_BOOT_ANIMATION_RES := 2160
+EXTRA_UDFPS_ANIMATIONS := true
+USE_MOTO_CALCULATOR := true
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+# Blur
+TARGET_ENABLE_BLUR := true
+# Pixel Stuff
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_CALL_RECORDING_SUPPORTED := true
+# Quick Tap
 TARGET_SUPPORTS_QUICK_TAP := true
