3fe44f444462e1933528d67b535abc50ce95df0f
Md Alam <151665023+mdalam073@users.noreply.github.com>
2025-01-29 10:08:56 +0600

    enable vendor partition and treble
    to check if the rom boots with treble enable

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,7 @@ TARGET_KERNEL_CONFIG := tissot_defconfig
 # A/B
 AB_OTA_PARTITIONS += \
     boot \
+    vendor \
     system
 
 # Partitions
@@ -39,6 +40,13 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
 
 # Security Patch Level
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+
+# Treble
+BOARD_VENDORIMAGE_PARTITION_SIZE := 629145600
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+PRODUCT_FULL_TREBLE_OVERRIDE := true
 
 # Inherit the proprietary files
 include vendor/xiaomi/tissot/BoardConfigVendor.mk
